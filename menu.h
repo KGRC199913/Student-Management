ec914696d76b101f6d2ede6afd66204166a2a2a3
haiyentr <37183335+haiyentr@users.noreply.github.com>
2018-04-19 16:22:47 +0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,7 +1,8 @@
 #pragma once
+enum STATE { UP, DOWN, LEFT, RIGHT, ENTER, BACK };
+STATE key(int z);
 int menu(int n);
 void frame();
 void addcourseframe(int x, int y, int z, int v, int j, int k, int m);
 int menucourse(int n);
-int menuaddcourse(int n);
 int menueditcourse(int n);
