5c84930bafb4c044b843bac06e0cc596aba22b9c
haiyentr <37183335+haiyentr@users.noreply.github.com>
2018-04-20 12:21:21 +0700

    Add files via upload

--- Score.cpp
+++ Score.cpp
@@ -2,11 +2,8 @@
 
 
 
-Score::Score(string code, int year, int semester, string ID, score_list score)
+Score::Score(string ID, score_list score)
 {
-	this->Course_code = code;
-	this->year = year;
-	this->semester = semester;
 	this->ID = ID;
 	this->score = score;
 }
@@ -14,10 +11,6 @@ Score::Score(string code, int year, int semester, string ID, score_list score)
 
 Score::~Score()
 {
-	this->Course_code.clear();
 	this->ID.clear();
 }
 
-void Score::print() {
-	cout << "Course code: " << this->Course_code << endl << "Year: " << this->year << endl << "Semester: " << this->semester << endl << "Score: " << endl << "\tMidterm: " << this->score.midterm << endl << "\tLab: " << this->score.lab << endl << "\tFinal: " << this->score.final << endl;
-}
--- Score.h
+++ Score.h
@@ -1,22 +1,27 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 struct score_list {
-	double midterm, lab, final;
+	double midterm, lab, final,bonus;
 };
 class Score
 {
+public:
+	string ID;
+	score_list score;
+	Score(string ID, score_list score);
+	~Score();
+};
+
+
+class Scoreboard {
+public:
 	string Course_code;
 	int year;
 	int semester;
-	string ID;
-	score_list score;
-public:
-	Score(string Code, int year, int semester, string ID, score_list score);
-	~Score();
-	void print();
+	vector<Score> data;
 };
-
