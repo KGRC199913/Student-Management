526a1d757bcd7d11229154df24339660b8b8c236
haiyentr <37183335+haiyentr@users.noreply.github.com>
2018-04-20 12:18:28 +0700

    Update menu.h

--- menu.h
+++ menu.h
@@ -6,3 +6,6 @@ void frame();
 void addcourseframe(int x, int y, int z, int v, int j, int k, int m);
 int menucourse(int n);
 int menueditcourse(int n);
+int st_menu(int n,User *user);
+int viewscore(int n, User *user, Scoreboard &temp, string course);
+int viewsch(int n, User *user, string path, vector<Course> &list);
